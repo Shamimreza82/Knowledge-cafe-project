6d22104c9a8fd26b871bb2893987a563ebf2b3f6
Shamimreza82 <shamimrezaone@gmail.com>
2023-09-11 22:29:38 +0600

    added json file component file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Header/Blogs/Blogs'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +11,8 @@ function App() {
   return (
     <>
       
-      <h1 className='text-6xl bg-red-500'>Knoledge Cafe</h1>
+     <Header></Header>
+     <Blogs></Blogs>
       
     </>
   )
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <div>
+             <h1 className='text-6xl bg-red-500'>Knoledge Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
